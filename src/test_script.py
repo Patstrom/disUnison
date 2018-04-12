2971271e93fca751b0bddcb2cec3517733164a10
Patrik Karlstrom <jp.karlstrom@gmail.com>
2018-04-12 18:10:10 +0000

    Update test script to test different strategies

--- src/test_script.py
+++ src/test_script.py
@@ -1,14 +1,32 @@
+#!/usr/bin/python3
 import os, sys, json
+from tqdm import tqdm
 
-path = sys.argv[1]
-
+test = sys.argv[1]
+path = sys.argv[2]
 json_files = [json.load(open(os.path.join(path,f))) for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
 
-register_permutations = [j["registers"] for j in json_files]
 
-for permutation in register_permutations:
-	occurences = register_permutations.count(permutation)
-	if occurences != 1:
-		print("{} occurs {} times".format(permutation, occurences))
-		break
+def test_schedule():
+	schedules = [(j["instructions"],j["cycles"]) for j in json_files]
+	for schedule in tqdm(schedules):
+		occurences = schedules.count(schedule)
+		if occurences != 1:
+			print("{} occurs {} times".format(schedule, occurences))
+			break
 
+
+def test_registers():
+	registers = [j["registers"] for j in json_files]
+	for register in tqdm(registers):
+		occurences = registers.count(register)
+		if occurences != 1:
+			print("{} occurs {} times".format(register, occurences))
+			break
+
+function = {
+	"registers": test_registers,
+	"schedule": test_schedule,
+}
+
+function[test]()
